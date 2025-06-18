27a0fddaa732584fae4694d4c4b00aea4bf8a975
Everton Luis <evertonluis.pro@outlook.com>
2025-06-18 01:36:52 -0300

    introduction to array

---
+++ javaScript_I/aula_08_array/array.js
@@ -0,0 +1,23 @@
+/*
+Estrutura de Dados
+
+1.Array -> vetor
+
+*/
+
+const users = [
+  {
+    name: "Rodolfo",
+    age: 30,
+    nomeDoConjuge: "Aline",
+    id: null,
+  },
+  {
+    name: "Aline",
+    age: 27,
+    nomeDoConjuge: "Rodolfo",
+    id: null,
+  },
+];
+
+console.log(users[1].age)
