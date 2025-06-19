c94d9312247c2ece6c2793f145849151edd10a88
Everton Luis <evertonluis.pro@outlook.com>
2025-06-19 17:51:41 -0300

    learning to use querySelector

---
+++ javaScript_II/aula_06_querySelector/querySelector.js
@@ -0,0 +1,15 @@
+/*
+Document -> HTML
+
+getElementById -> Trás UM elemento pelo ID
+getElementByClassName -> Trás TODOS os elementos com essa classe
+getElementByTagName -> Trás TODOS os elementos com essa TAG
+getElementByName -> Trás TODOS os elementos com esse NAME
+
+querySelector -> Trás UM elemtno, o PRIMEIRO que encontrar
+querySelectorAll -> Trás TODOS os elemtnos que ecnontrar
+*/
+
+const elements = document.querySelector(".paragraph-js")
+
+console.log(elements)
