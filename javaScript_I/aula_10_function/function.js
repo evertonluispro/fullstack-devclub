69e9a297a6e5761cee2d5214b3a9a5bfbcbb7ba7
Everton Luis <evertonluis.pro@outlook.com>
2025-06-19 13:40:51 -0300

    introduction to functions

---
+++ javaScript_I/aula_10_function/function.js
@@ -0,0 +1,15 @@
+/*
+Functions / Funções
+    Trecho de código que é executado
+    SOMENTE quando solicitado ( Parente gente boa )
+*/
+
+const variavel = "abacate"
+
+console.log(variavel)
+
+function nomeNaTela() {
+    console.log("Rodolfo")
+}
+
+nomeNaTela()
