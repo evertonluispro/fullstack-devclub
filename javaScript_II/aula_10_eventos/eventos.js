8146c3c1834c064af73217e7d7e18bb4e00d7f75
Everton Luis <evertonluis.pro@outlook.com>
2025-06-21 19:48:40 -0300

    Trabalhando com eventos

---
+++ javaScript_II/aula_10_eventos/eventos.js
@@ -0,0 +1,13 @@
+/*
+eventos
+*/
+
+const input = document.querySelector("#main-input")
+
+function cliqueiBotao(){
+    console.log(input.value)
+}
+
+function digiteiNoInput(){
+    console.log("Digitei no input")
+}
