e6f69270abb5808d2e3056c5fe532390983dbf96
Everton Luis <evertonluis.pro@outlook.com>
2025-06-24 00:08:14 -0300

    Operadores aritmeticos

---
+++ javaScript_III/aula_03_operadores_aritmeticos/operadores-aritmeticos.js
@@ -0,0 +1,20 @@
+/*
+
+Operadores Aritméticos
+
+    + -> Adição
+    - -> Subtração
+    / -> Divisão
+    * -> Multplicação
+    % -> Resto
+    ++ -> Incremento
+    -- -> Decremento
+    ** -> Exponencial
+
+*/
+
+const result = 20
+
+result ++
+
+console.log(result)
