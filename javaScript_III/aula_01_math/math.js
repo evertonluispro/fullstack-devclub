9f5ad99249981e27aad42270e2dc9ec48bde8242
Everton Luis <evertonluis.pro@outlook.com>
2025-06-23 22:39:35 -0300

    aprendendo sobre o Math

---
+++ javaScript_III/aula_01_math/math.js
@@ -0,0 +1,16 @@
+/*
+
+Math -> Matemática
+    - pow(2,2) / potência / 2 = 4
+    - sqrt(25,2) / raiz / √25
+    - PI
+    - ceil / teto -> arredondar pra cima
+    - floor / chão -> arredondar para baixo
+
+    - random() / número aleatório entre 0 e 1
+
+*/
+
+const result = Math.ceil(3.14)
+
+console.log(result)
