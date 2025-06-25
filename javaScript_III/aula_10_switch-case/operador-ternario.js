70a3fef7eec12a98e61299e519bc7bc8040bfeb5
Everton Luis <evertonluis.pro@outlook.com>
2025-06-25 17:38:06 -0300

    Setimeout

---
+++ javaScript_III/aula_10_switch-case/operador-ternario.js
@@ -0,0 +1,27 @@
+/*
+
+IF ELSE
+
+    Switche case
+
+*/
+
+const temperature = 30;
+
+switch (key) {
+  case 10:
+    console.log("Ta meio frio")
+    break;
+
+  case 20:
+    console.log("Ta meio quente")
+    break;
+
+  case 30:
+    console.log("Ta quente")
+    break;
+
+  default:
+    console.log("Não sei te dizer")
+    break;
+}
