ed630621534fc0fc989351fe7c5231e51a682255
Everton Luis <evertonluis.pro@outlook.com>
2025-06-25 17:23:03 -0300

    Operadores ternório

---
+++ javaScript_III/aula_09_operador_ternario/operador-ternario.js
@@ -0,0 +1,18 @@
+/*
+
+IF ELSE
+
+    ? IF -> SE
+    : Else -> Se não
+
+*/
+
+const salary = 3000
+
+//if(salary < 3000) {
+ //   console.log("O colaborador é Junior")
+//} else {
+//    console.log("ele é Senior")
+//}
+
+salary < 3000 ? console.log("O colaborador é Junior") : console.log("ele é Senior")
