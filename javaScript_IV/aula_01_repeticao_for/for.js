07bb70bfd2025c64ec3b339f918121bd5550ea7f
Everton Luis <evertonluis.pro@outlook.com>
2025-06-26 20:22:39 -0300

    Repetição for

---
+++ javaScript_IV/aula_01_repeticao_for/for.js
@@ -0,0 +1,20 @@
+/*
+
+Estrutura de Repetição - LOOP
+
+    - FOR
+        1) INICIALIZAÇÃO - Criar uma variável e colocar um valor inicial pra ela
+        2) CONDIÇÃO - Enquanto for true, o laço continuará iterando.
+                    Ele irá verificar antes de cada iteração
+        3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que o nosso laço de uma volta
+
+        for ([inicialização]; [condição]; [expressão final]) {
+            Seu código aqui
+        }
+
+*/
+
+for(let i = 0; i < 15; i++){
+    console.log(i)
+}
+
