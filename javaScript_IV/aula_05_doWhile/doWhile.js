d8cde94521a4c0ca73b00006e432239ce1e65e86
Everton Luis <evertonluis.pro@outlook.com>
2025-06-30 21:18:44 -0300

    Repetições usando doWhile e ForEach

---
+++ javaScript_IV/aula_05_doWhile/doWhile.js
@@ -0,0 +1,16 @@
+/*
+
+    while & do while
+
+    while -> Verifique se é TRUE e Faça
+    do while -> Faça, depois veja se é true
+
+*/
+
+let i = 0
+console.log("Um código aqui")
+
+do {
+    i++;
+    console.log(i)
+} while (i < 10);
