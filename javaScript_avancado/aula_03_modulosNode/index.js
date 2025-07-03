7a2f9a58ae06d39e48dcba422b76f9821339bd0e
Everton Luis <evertonluis.pro@outlook.com>
2025-07-03 15:22:20 -0300

    Passando por modulos Node

---
+++ javaScript_avancado/aula_03_modulosNode/index.js
@@ -0,0 +1,5 @@
+const { sum, multi, person} = require('./objectAndFunctions');
+
+console.log(sum(2,2))
+console.log(multi(2,5))
+console.log(person)
---
+++ javaScript_avancado/aula_03_modulosNode/objectAndFunctions.js
@@ -0,0 +1,14 @@
+
+function multi(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function sum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
+
+const person = {
+    name: "Rodolfo"
+}
+
+module.exports = { multi, sum, person };
