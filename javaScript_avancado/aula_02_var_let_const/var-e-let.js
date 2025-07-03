cf4002b5f11c609514b089fd067cfb741a68e18a
Everton Luis <evertonluis.pro@outlook.com>
2025-07-03 14:59:16 -0300

    As princiapis diferenças entre var, let e const

---
+++ javaScript_avancado/aula_02_var_let_const/var-e-let.js
@@ -0,0 +1,13 @@
+/* VAR x LET x CONST
+*/
+
+var myVar = "Hello" // NÃO USAMOS MAIS O VAR
+let myLet = "Olá" // PODE TER O VALOR ALTERADO
+const myObject = [5,6,7] // PODE SER ALTERADO APENAS VALORES INTERNOS
+
+myLet = "Mundo" // PODE SER REATRIBUÍDO
+myObject[0] = 6 // PODE SER ALTERADO O VALOR INTERNO DO ARRAY
+
+
+
+console.log(myObject)
